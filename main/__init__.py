6cd82024e4bdde900924072321b43ab825fdfc33
orhogi <96086982+orhogi@users.noreply.github.com>
2024-07-19 05:45:54 +0300

    __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("16077282", default=None, cast=int)
+API_HASH = config("de1f19d1abbaeada763cdbc22a055530", default=None)
+BOT_TOKEN = config("6698534371:AAER-R5hc6bNBF60KO82LmwNkzqNelfVmHY", default=None)
+SESSION = config("BAD1UeIAVD1jx0pfeRKpJE9_fdqPk4NncYfjfrQ2QGE2O5kHdP0TpijsBJUcQAcVuhfEm6aXx7YgIT0uvvWAe9jJGxuSSf2d0kUe1NIbAJd9k6IgoXmAcvBk2EiRa4YBUNllw9GwLlunsLIXaNYzNcV12ht1I47zD471vGTHr_3TRiuAEO7Fdl9ADQFL8S-sIUCSRmJvo-H6Grqdg74hdpnrRC3VtmJbp1or1xP_CobVTKtTPdjDGvyjeJolzcFp-KvI8ePHOaifwAeKPw2Tr4F_XO0jJNwvssj0zLXAzcOPHjY51hgO_3gqdgUdF-TSyjk5ZPgJ4Q1F5MlXJ4XuCHMip5f40AAAAAFlRRRxAA", default=None)
+FORCESUB = config("savingahhbot12", default=None)
+AUTH = config("5993993329", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
